478fcc95fdabc0a9d2c5504f7da2847be5c7e8a8
I041941 <I041941@W-PF42FGKH>
2024-02-12 20:42:55 +0100

    Remove usage of AspectJ

--- src/main/java/com/sap/grc/iag/springboot/config/WebAppContextConfig.java
+++ src/main/java/com/sap/grc/iag/springboot/config/WebAppContextConfig.java
@@ -3,14 +3,13 @@ package com.sap.grc.iag.springboot.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-@EnableAspectJAutoProxy(proxyTargetClass = true)
+//@EnableAspectJAutoProxy(proxyTargetClass = true)
 @ComponentScan(basePackages = { "com.sap.grc.iag.springboot" })
 public class WebAppContextConfig implements WebMvcConfigurer {
 
